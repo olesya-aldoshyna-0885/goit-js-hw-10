5ec3010cf760c655909ce08ec7a473de6507de59
olesya-aldoshyna-0885 <109144189+olesya-aldoshyna-0885@users.noreply.github.com>
2023-01-26 17:31:31 +0000

    Deploying to gh-pages from @ olesya-aldoshyna-0885/goit-js-hw-10@15e6edd6d84e1c6fe56874b2f2060f68a6b0dac4 🚀

---
+++ index.695f0d67.js
@@ -0,0 +1,2 @@
+getRefs().inputEl.addEventListener("input",debounce(onInput,300));
+//# sourceMappingURL=index.695f0d67.js.map
---
+++ index.f6733546.js
@@ -0,0 +1,2 @@
+getRefs().inputEl.addEventListener("input",debounce(onInput,300));
+//# sourceMappingURL=index.f6733546.js.map
